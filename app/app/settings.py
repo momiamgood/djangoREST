13327bce3444d5f2acb76ad6ae3100568213cd54
momiamgood <100787969+momiamgood@users.noreply.github.com>
2023-01-29 22:41:32 +0700

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'django_filters',
     'main',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
